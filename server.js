ec5a42b7e450171dab269aebc2c0a82644eef51e
ekanshiag <ekanshi001@gmail.com>
2018-09-11 14:34:10 +0530

    Parse Body

--- server.js
+++ server.js
@@ -3,6 +3,54 @@ let req = {}
 
 function checkForHeaders (buffer) { return buffer.toString().includes('\r\n\r\n') }
 function splitLines (data, ch) { return data.split(ch) }
+function wantBody () { return req.method === 'POST' }
+function checkContentLenHeader () { return req.headers.hasOwnProperty('Content-Length') }
+function checkBodyLength (buffer) {
+  return Buffer.byteLength(buffer) === Number(req.headers['Content-Length'])
+    ? 0
+    : Buffer.byteLength(buffer) > Number(req.headers['Content-Length'])
+      ? 1
+      : -1
+}
+
+function verifyAndGetBody (buffer, client) {
+  switch (checkBodyLength(buffer)) {
+    case 0: parseBody(buffer)
+      break
+    case 1: closeConnection(client, '413')
+      break
+    case -1: waitForBody(client)
+      break
+  }
+}
+
+function closeConnection (client, status) {
+  client.write('Closing connection with status ' + status)
+  client.end()
+}
+
+function waitForBody (client) {
+  client.setTimeout(3000)
+}
+
+function parseBody (buffer) {
+  switch (req.headers['Content-Type']) {
+    case 'application/json': req.body = JSON.parse(buffer.toString())
+      break
+    case 'application/x-www-form-urlencoded': req.body = parseUrl(buffer)
+      break
+  }
+}
+
+function parseUrl (buffer) {
+  let obj = {}
+  let args = splitLines(buffer.toString(), '&')
+  args.forEach(arg => {
+    let x = splitLines(arg, '=')
+    obj[x[0]] = x[1]
+  })
+  return obj
+}
 
 function makeRequestObject () {
   req = {
@@ -20,7 +68,8 @@ function setUpConnection () {
     console.log('Client connected')
     client.on('error', err => console.error(err))
     client.on('end', () => console.log('Client connection ended'))
-    client.on('data', datachunk => handleConnection(datachunk))
+    client.on('data', datachunk => handleConnection(datachunk, client))
+    client.on('timeout', () => closeConnection(client, '408'))
   })
   server.listen(3000)
 }
@@ -45,6 +94,11 @@ function parseHeaders (buffer) {
   }
 }
 
+function normaliseHeaders (buffer) {
+  return Buffer.from(buffer.toString().replace('content-type', 'Content-Type')
+    .replace('content-length', 'Content-Length'))
+}
+
 function pipe (...fns) {
   return function (buffer) {
     fns.forEach(fn => {
@@ -53,15 +107,26 @@ function pipe (...fns) {
   }
 }
 
-let getHeaders = pipe(parseReqLine, separateHeaders, parseHeaders)
+let getHeaders = pipe(parseReqLine, separateHeaders, normaliseHeaders, parseHeaders)
 let dataBuffer = Buffer.from([])
+let getBody = 0
 
-function handleConnection (datachunk) {
+function handleConnection (datachunk, client) {
   dataBuffer += datachunk
-  if (checkForHeaders(dataBuffer)) {
+  if (checkForHeaders(dataBuffer) && getBody === 0) {
     getHeaders(dataBuffer)
-    console.log(req)
+    if (wantBody()) {
+      let headersEnd = dataBuffer.indexOf('\r\n\r\n')
+      dataBuffer = dataBuffer.slice(headersEnd + 4)
+      getBody = 1
+    }
   }
+  if (getBody === 1) {
+    checkContentLenHeader()
+      ? verifyAndGetBody(dataBuffer, client)
+      : closeConnection(client, '411')
+  }
+  console.log(req)
 }
 
 setUpConnection()
