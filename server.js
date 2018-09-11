7a250532a2d1330aea2c6616b598b381d87e0249
ekanshiag <ekanshi001@gmail.com>
2018-09-11 11:36:29 +0530

    Parse headers

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const net = require('net')
 let req = {}
 
+function checkForHeaders (buffer) { return buffer.toString().includes('\r\n\r\n') }
+function splitLines (data, ch) { return data.split(ch) }
+
 function makeRequestObject () {
   req = {
     method: '',
@@ -13,19 +16,52 @@ function makeRequestObject () {
 
 function setUpConnection () {
   const server = net.createServer(client => {
-    client.on('connection', () => {
-      makeRequestObject()
-      console.log('Client connected')
-    })
+    makeRequestObject()
+    console.log('Client connected')
     client.on('error', err => console.error(err))
-    client.on('end', console.log('Client connection ended'))
+    client.on('end', () => console.log('Client connection ended'))
     client.on('data', datachunk => handleConnection(datachunk))
   })
   server.listen(3000)
 }
 
-function handleConnection (datachunk) {
+function parseReqLine (buffer) {
+  let reqLine = splitLines(buffer.toString(), '\r\n')[0]
+  let length = reqLine.length
+  reqLine = splitLines(reqLine, ' ')
+  req.method = reqLine[0]
+  req.uri = reqLine[1]
+  req.version = reqLine[2]
+  return buffer.slice(length + 2)
+}
 
+function separateHeaders (buffer) { return buffer.slice(0, buffer.indexOf('\r\n\r\n')) }
+
+function parseHeaders (buffer) {
+  let headers = splitLines(buffer.toString(), '\r\n')
+  for (let header of headers) {
+    let x = splitLines(header, ':')
+    req.headers[x.shift()] = x.join(':').trim()
+  }
+}
+
+function pipe (...fns) {
+  return function (buffer) {
+    fns.forEach(fn => {
+      buffer = fn(buffer)
+    })
+  }
+}
+
+let getHeaders = pipe(parseReqLine, separateHeaders, parseHeaders)
+let dataBuffer = Buffer.from([])
+
+function handleConnection (datachunk) {
+  dataBuffer += datachunk
+  if (checkForHeaders(dataBuffer)) {
+    getHeaders(dataBuffer)
+    console.log(req)
+  }
 }
 
 setUpConnection()
